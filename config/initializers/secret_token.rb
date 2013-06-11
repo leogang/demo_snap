8d0877f50684394cdc0d361e4fac386f7af2cade
leogang <leogang89@gmail.com>
2013-06-11 11:29:39 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoSnap::Application.config.secret_token = 'e34906711181abe60d2ecfe07afe8269963d509ef83c3030a3487c47683127eebb6daef619b685fab4e327b5b53bddfabd92a6cc67eb382f07e4aa44164eab99'
